54a977a7dc1a15456ba2fce9b2ba4d352ede601a
Yannis Marketakis <yannismarketakis@gmail.com>
2024-03-14 15:11:42 +0200

    removed sampleperson related resources

--- src/main/java/gr/forth/ics/isl/Application.java
+++ src/main/java/gr/forth/ics/isl/Application.java
@@ -3,13 +3,8 @@ package gr.forth.ics.isl;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
-import gr.forth.ics.isl.data.SamplePersonRepository;
-import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDatabaseInitializer;
-import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
-import org.springframework.context.annotation.Bean;
 
 /**
  * The entry point of the Spring Boot application.
@@ -21,23 +16,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @Theme(value = "easylink", variant = Lumo.DARK)
 public class Application implements AppShellConfigurator {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    SqlDataSourceScriptDatabaseInitializer dataSourceScriptDatabaseInitializer(DataSource dataSource,
-            SqlInitializationProperties properties, SamplePersonRepository repository) {
-        // This bean ensures the database is only initialized when empty
-        return new SqlDataSourceScriptDatabaseInitializer(dataSource, properties) {
-            @Override
-            public boolean initializeDatabase() {
-                if (repository.count() == 0L) {
-                    return super.initializeDatabase();
-                }
-                return false;
-            }
-        };
     }
 }
