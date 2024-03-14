6adf845ca2adbcb41e312e66f0d828d8a99fef65
Yannis Marketakis <yannismarketakis@gmail.com>
2024-03-14 21:16:37 +0200

    first draft implementation of resolver

---
+++ src/main/java/gr/forth/ics/isl/rest/UrlResourceController.java
@@ -0,0 +1,42 @@
+package gr.forth.ics.isl.rest;
+
+import gr.forth.ics.isl.data.UrlResource;
+import gr.forth.ics.isl.data.UrlResourceRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Yannis Marketakis (marketak 'at' forth 'dot' ics 'dot' gr)
+ */
+@RestController
+public class UrlResourceController{
+
+    private final UrlResourceRepository repository;
+
+    public UrlResourceController(UrlResourceRepository repo){
+        this.repository=repo;
+    }
+
+    @GetMapping("/urlresources")
+    public void all(){
+        List<UrlResource> allResources=this.repository.findAll();
+        System.out.println(allResources.size());
+    }
+
+    @RequestMapping(value = "/r/{suffix}", method = RequestMethod.GET)
+    public void method(HttpServletResponse httpServletResponse, @PathVariable String suffix) {
+        System.out.println("search for url with suffix "+suffix);
+        httpServletResponse.setHeader("Location", "https://www.google.com");
+        httpServletResponse.setStatus(302);
+    }
+
+    /**
+     * Yannis Marketakis (marketak 'at' forth 'dot' ics 'dot' gr)
+     */
+    public static class UrlResourceNotFoundException extends RuntimeException{
+
+
+    }
+}
