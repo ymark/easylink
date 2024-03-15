c8336c906aaeb992eb7f93aadfb88b221b9cb7bf
Yannis Marketakis <yannismarketakis@gmail.com>
2024-03-15 11:54:26 +0200

    removed debug messages

--- src/main/java/gr/forth/ics/isl/rest/UrlResourceController.java
+++ src/main/java/gr/forth/ics/isl/rest/UrlResourceController.java
@@ -2,13 +2,9 @@ package gr.forth.ics.isl.rest;
 
 import gr.forth.ics.isl.data.EntityManager;
 import gr.forth.ics.isl.data.UrlResource;
-import gr.forth.ics.isl.data.UrlResourceRepository;
 import gr.forth.ics.isl.services.UrlResourceService;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -25,7 +21,6 @@ public class UrlResourceController{
     @RequestMapping(value = "/r/{suffix}", method = RequestMethod.GET)
     public void method(HttpServletResponse httpServletResponse, @PathVariable String suffix) {
         Optional<UrlResource> optUrlResource=service.findByUrl(EntityManager.EASY_URL_PREFIX+suffix,true);
-        System.out.println("Found it "+optUrlResource.isPresent());
         if(optUrlResource.isPresent()){
             UrlResource usedUrlResource=optUrlResource.get().visit();
             service.update(usedUrlResource);
