eb69c9982d8c320f29802d45ccbecb4b15a66ac2
Yannis Marketakis <yannismarketakis@gmail.com>
2024-03-15 17:02:38 +0200

    Update UrlResourceController.java
    minor improvements

--- src/main/java/gr/forth/ics/isl/rest/UrlResourceController.java
+++ src/main/java/gr/forth/ics/isl/rest/UrlResourceController.java
@@ -6,7 +6,6 @@ import gr.forth.ics.isl.services.UrlResourceService;
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.*;
 import java.util.Optional;
 
@@ -37,11 +36,10 @@ public class UrlResourceController{
 
     @RequestMapping(value = "/qr/{id}", method = RequestMethod.GET)
     public void methodQr(HttpServletResponse httpServletResponse, @PathVariable String id) {
-//        String filePath = "qr/GsPihS1byO.png";  //to change
         String filePath=EntityManager.QR_FOLDER+"/"+id;
         File file = new File(filePath);
         httpServletResponse.setContentType("image/png");
-        httpServletResponse.setHeader("Content-Disposition", "inline; filename=image.png"); //to change filename
+        httpServletResponse.setHeader("Content-Disposition", "inline; filename="+id);
         httpServletResponse.setContentLength((int)file.length());
         byte[] imageData=new byte[(int)file.length()];
         try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
@@ -49,7 +47,6 @@ public class UrlResourceController{
         }catch(IOException ex){
             System.out.println("fnf "+ex.toString());
         }
-        // Write the byte array to the response output stream
         try (ServletOutputStream outputStream = httpServletResponse.getOutputStream()) {
             outputStream.write(imageData);
         }
