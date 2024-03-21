ed87bc6e8edc6f05a5d73393e275507d73f51e64
Yannis Marketakis <yannismarketakis@gmail.com>
2024-03-21 13:15:40 +0200

    removed commented and unused code

--- src/main/java/gr/forth/ics/isl/views/create/CreateView.java
+++ src/main/java/gr/forth/ics/isl/views/create/CreateView.java
@@ -134,60 +134,6 @@ public class CreateView extends VerticalLayout {
         }
     }
 
-//    private void updateResultsPanel(UrlResource urlResource){
-//        this.resultsPanelLayout.removeAll();
-//        this.resultsPanelLayout.setHeight("600px");
-//
-//        H2 easyUrlComponent=new H2();
-//        TextArea orTextArea=new TextArea("Original URL");
-//        TextField nmTextField=new TextField("Name");
-//        TextArea dsTextArea=new TextArea("Description");
-//        DatePicker crDate=new DatePicker("Created on");
-//        DatePicker luDate=new DatePicker("Last Used");
-//        TextField vsTextField=new TextField("Visits");
-//
-//        Button copyUrlButton=new Button(VaadinIcon.COPY.create());
-//        copyUrlButton.addClickListener(e ->
-//                {
-//                    UI.getCurrent().getPage().executeJs("navigator.clipboard.writeText($0);", easyUrlComponent.getText());
-//                    Notification.show("Value copied to clipboard",4000,Notification.Position.TOP_END);
-//                }
-//        );
-//        copyUrlButton.setMaxWidth("25px");
-//
-//        easyUrlComponent.setText(urlResource.getEasyUrl());
-//        orTextArea.setValue(urlResource.getOriginalUrl());
-//        orTextArea.setReadOnly(true);
-//        orTextArea.setMaxHeight("100px");
-//        nmTextField.setValue(urlResource.getName());
-//        nmTextField.setReadOnly(true);
-//        dsTextArea.setValue(urlResource.getDescription());
-//        dsTextArea.setReadOnly(true);
-//        dsTextArea.setMaxHeight("100px");
-//        crDate.setValue(urlResource.getCreatedDateLocal());
-//        crDate.setReadOnly(true);
-//        if(urlResource.getLastUsed()!=null){
-//            luDate.setValue(urlResource.getLastUsedDateLocal());
-//        }
-//        luDate.setReadOnly(true);
-//        vsTextField.setValue(String.valueOf(urlResource.getVisited()));
-//        vsTextField.setReadOnly(true);
-//
-//        HorizontalLayout easyUrlLayout=new HorizontalLayout();
-//        easyUrlLayout.add(easyUrlComponent,copyUrlButton);
-//        easyUrlLayout.setAlignItems(Alignment.END);
-//
-//        FormLayout detailsFormLayout=new FormLayout();
-//        detailsFormLayout.setResponsiveSteps(new FormLayout.ResponsiveStep("0",3));
-//        detailsFormLayout.setColspan(easyUrlLayout,3);
-//        detailsFormLayout.setColspan(orTextArea,3);
-//        detailsFormLayout.setColspan(nmTextField,3);
-//        detailsFormLayout.setColspan(dsTextArea,3);
-//        detailsFormLayout.add(easyUrlLayout,orTextArea,nmTextField,dsTextArea,crDate,luDate,vsTextField);
-//
-//        this.resultsPanelLayout.add(detailsFormLayout);
-//    }
-
     private void notifyMessage(String message, NotificationVariant nVariant){
         Notification notification=new Notification();
         notification.addThemeVariants(nVariant);
