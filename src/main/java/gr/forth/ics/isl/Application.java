399858b7d6aacec77ba87e2ff03f85aa1902b91e
Yannis Marketakis <yannismarketakis@gmail.com>
2024-07-04 14:53:39 +0300

    added favicon

--- src/main/java/gr/forth/ics/isl/Application.java
+++ src/main/java/gr/forth/ics/isl/Application.java
@@ -1,6 +1,7 @@
 package gr.forth.ics.isl;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.AppShellSettings;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
@@ -18,4 +19,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application implements AppShellConfigurator {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }}
+    }
+    
+    @Override
+    public void configurePage(AppShellSettings settings){
+        settings.addFavIcon("icon", "icons/icon-192.png", "192x192");
+        settings.addLink("shortcut icon","icons/favicon.ico");
+    }
+
+}
