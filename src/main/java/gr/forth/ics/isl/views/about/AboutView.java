ec9e99952ac1f126e2ab0cf0b9dc84eeccc1e964
Yannis Marketakis <yannismarketakis@gmail.com>
2024-07-11 15:16:06 +0300

    about view various fixes

--- src/main/java/gr/forth/ics/isl/views/about/AboutView.java
+++ src/main/java/gr/forth/ics/isl/views/about/AboutView.java
@@ -8,6 +8,7 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.ListItem;
+import com.vaadin.flow.component.html.UnorderedList;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
@@ -25,10 +26,7 @@ public class AboutView extends VerticalLayout {
     
     private void addLogo(){
         HorizontalLayout logoLayout=new HorizontalLayout();
-        Image img = new Image("images/easylink-256.png", "EasyLink");
-        img.setWidth("250px");;
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
+        Image img = new Image("images/easylink-150.png", "EasyLink");
         setDefaultHorizontalComponentAlignment(Alignment.AUTO);
         getStyle().set("text-align", "justify");
         logoLayout.add(img,new H1("About EasyLink"));
@@ -46,14 +44,15 @@ public class AboutView extends VerticalLayout {
                 + "Our platform offers a suite of features designed to simplify and enhance your link-sharing experience "
                 + "and provide with various analytics for monitoring your easy links");
         H2 keyFearuresHeader=new H2("Key Features");
-        ListItem keyFeature1=new ListItem("Easily convert long and unwieldy URLs into concise, easy-to-share links. Perfect for social media, emails, and other communication channels.");
-        ListItem keyFeature2=new ListItem("For every link you create, EasyLink automatically generates a unique QR code. This makes it easy to share links in print materials, presentations, or anywhere a QR code can be scanned");
-        ListItem keyFeature3=new ListItem("Gain valuable insights into your link's performance. Track metrics such as the number of clicks, geographic location of visitors, referral sources, and more. Use this data to understand your audience and optimize your link-sharing strategy.");
-        ListItem keyFeature4=new ListItem("Choose the suffix of your easy link. Using custom links you can create an easy to remember URL.");
-        ListItem keyFeature5=new ListItem("Create easy links with an expiration date. By default easy links will live forever, unless you define an expiration date for them.");
-        ListItem keyFeature6=new ListItem("Did you forgot your easy link? No worries. Simply search (or create an easy link) for your long URL and you'll retrieve your original easy link");
-        ListItem keyFeature7=new ListItem("Add the easylink bookmarklet in your favorite browser, and your easy links are just one click away. Just drag'd'drop the EasyLink bookmarklet (bottom left of the screen) in your bookmarks bar");
-        ListItem keyFeature8=new ListItem("Your easy links will live forever. There are no expiration dates (by default) or usage quotas.");
+        UnorderedList featuresList=new UnorderedList();
+        featuresList.add(new ListItem("Easily convert long and unwieldy URLs into concise, easy-to-share links. Perfect for social media, emails, and other communication channels."));
+        featuresList.add(new ListItem("For every link you create, EasyLink automatically generates a unique QR code. This makes it easy to share links in print materials, presentations, or anywhere a QR code can be scanned"));
+        featuresList.add(new ListItem("Gain valuable insights into your link's performance. Track metrics such as the number of clicks, geographic location of visitors, referral sources, and more. Use this data to understand your audience and optimize your link-sharing strategy."));
+        featuresList.add(new ListItem("Choose the suffix of your easy link. Using custom links you can create an easy to remember URL."));
+        featuresList.add(new ListItem("Create easy links with an expiration date. By default easy links will live forever, unless you define an expiration date for them."));
+        featuresList.add(new ListItem("Did you forgot your easy link? No worries. Simply search (or create an easy link) for your long URL and you'll retrieve your original easy link"));
+        featuresList.add(new ListItem("Add the easylink bookmarklet in your favorite browser, and your easy links are just one click away. Just drag'd'drop the EasyLink bookmarklet (bottom left of the screen) in your bookmarks bar"));
+        featuresList.add(new ListItem("Your easy links will live forever. There are no expiration dates (by default) or usage quotas."));
         
 //        H2 whyChooseHeader=new H2("Why Choose EasyLink:");
 //        ListItem whyChooseItem1=new ListItem("Our intuitive design ensures that anyone can start shortening URLs and generating QR codes in seconds, with no technical expertise required.");
@@ -72,7 +71,7 @@ public class AboutView extends VerticalLayout {
         add(
 //                aboutHeader,
                 contentsDiv,
-                keyFearuresHeader,keyFeature1,keyFeature2,keyFeature3,keyFeature4,keyFeature5,keyFeature6,keyFeature7,keyFeature8,
+                keyFearuresHeader,featuresList,
 //                whyChooseHeader,whyChooseItem1,whyChooseItem2,whyChooseItem3,
 //                getStartedHeader,getStartedDiv,
                 questionsAndFeedbackHeader,questionsAndFeedbackDiv);
